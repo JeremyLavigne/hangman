929d45a2a0b8aa0840fd08781d4150a58a055ec4
Jeremy <lavigne.jeremy@post.com>
2020-08-19 20:53:28 +0200

    Add win/lose management

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,32 +1,77 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Keyboard from './Keyboard'
 
 const Game = ({language, secretWord}) => {
 
     const word = secretWord.toUpperCase()
-    const [ displayedWord, setDisplayedWord ] = useState(word.split('').map(char => '_ '))
-    const [ discoveredLetters, setDiscoveredLetters ] = useState([])
-
     console.log('Word to discover : ', word)
 
+    const [ displayedWord, setDisplayedWord ] = useState(word.split('').map(char => '_ '))
+    const [ discoveredLetters, setDiscoveredLetters ] = useState([])
+    const [ missedLetters, setMissedLetters ] = useState([])
+    const [ count, setCount ] = useState(7)
+    const [ gameIsLost, setGameIsLost ] = useState(false)
+    const [ gameIsWon, setGameIsWon ] = useState(false)
+
+    useEffect(() => {
+        if (count === 0) {
+            setGameIsLost(true)
+            console.log('JULES TU PUES ET EN PLUS TES MOCHE')
+        }
+
+        if (!displayedWord.includes('_ ')){
+            setGameIsWon(true)
+        }
+    }, [count, displayedWord])
+
+    // Deal with every click
     const clickOnALetter = (letter) => {
         console.log('you clicked on ', letter)
 
-        if (word.split('').includes(letter)) {
-            setDiscoveredLetters(discoveredLetters.concat(letter))
-            setDisplayedWord(word.split('').map(l => (discoveredLetters.includes(l) || l === letter ) ? l : '_ '))
+        if (gameIsLost) {
+            return null
         }
 
-        console.log(discoveredLetters)
+        if (gameIsWon) {
+            console.log('You win')
+            return null
+        }
+
+        // If the clicked letter is in the word
+        if (word.split('').includes(letter)) {
+            // Add it to the Array of already discovered letters
+            setDiscoveredLetters(discoveredLetters.concat(letter))
+            setDisplayedWord(word.split('').map(l => (discoveredLetters.includes(l) || l === letter ) ? l : '_ '))
+
+        } else {
+            setMissedLetters(missedLetters.concat(letter))
+            setCount(count-1)
+        }
     }
+
+
 
     return (
         <div>
+            <div>
+                <p>Reamining moves : {count}</p>
+            </div>
+
             <div className="is-size-2 has-text-centered mt-6">
                 {displayedWord}
             </div>
 
-            <Keyboard language={language} onClick={clickOnALetter}/>
+            <Keyboard 
+                language={language} 
+                onClick={clickOnALetter}
+                discoveredLetters={discoveredLetters}
+                missedLetters={missedLetters}
+            />
+
+            <div className={"is-size-1 has-text-centered " + gameIsLost ? "has-text-danger" : "has-text-success"}>
+                {gameIsLost ? "You Lose" : null}
+                {gameIsWon ? "You Won" : null}
+            </div>
         </div>
     )
 }
--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Keyboard = ({language, onClick}) => {
+const Keyboard = ({language, onClick, discoveredLetters, missedLetters}) => {
 
     let alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' ]
     
@@ -8,11 +8,28 @@ const Keyboard = ({language, onClick}) => {
         alphabet = alphabet.concat(['Å', 'Ä', 'Ö' ])
     }
 
+    const style = {
+        success : {
+            backgroundColor : 'green'
+        },
+        failed : {
+            backgroundColor : 'red'
+        }
+    }
+
     return (
         <div className="has-text-centered mt-6">
             {alphabet.map(letter => 
-                <button className="button px-2 mx-1 my-1" key={letter}
-                    onClick={() => onClick(letter)}>
+                <button 
+                    className="button px-2 mx-1 my-1" 
+                    key={letter}
+                    onClick={() => onClick(letter)}
+                    style={
+                        discoveredLetters.includes(letter) ? style.success : 
+                        missedLetters.includes(letter) ? style.failed : null
+                    }
+                    disabled={(discoveredLetters.includes(letter) || missedLetters.includes(letter)) ? true : false}
+                    >
                     {letter}
                 </button>)
             }
